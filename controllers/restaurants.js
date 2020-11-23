076854f411f87695dfaa94df48ea450e696c799d
Imrane Abdoul <imraneabdoul@gmail.com>
2020-11-23 18:54:27 +0100

    add endpoint update Restaurant

--- controllers/restaurants.js
+++ controllers/restaurants.js
@@ -10,3 +10,29 @@ exports.createRestaurant = async(req,res,next) =>{
     }
 
 }
+
+exports.updateRestaurant= async(req,res,next)=>{
+    const updates = Object.keys(req.body)
+    console.log(updates)
+    const updateAllowed = ["name","address"]
+    const isValidOperation = updates.every((update)=> updateAllowed.includes(update))
+
+    if(!isValidOperation){
+        return res.status(404).json({message:"Restaurant not found to update"})
+    }
+
+    try{
+        const restaurantToUpdate = await Restaurant.findById(req.params.id)
+        updates.forEach(update =>{
+            restaurantToUpdate[update]=req.body[update]
+        })
+        await restaurantToUpdate.save()
+
+        if(!restaurantToUpdate){
+            return res.status(404).json({message:"Restaurant not found to update"})
+        }
+        res.status(200).json({message:"Updated restaurant!"})
+    }catch(e){
+        res.status(500).json({message:e})
+    }
+}
--- routes/restaurants.js
+++ routes/restaurants.js
@@ -5,5 +5,6 @@ const auth = require("../middleware/auth")
 const restaurantsCtrl = require("../controllers/restaurants")
 
 router.post("/create",auth,restaurantsCtrl.createRestaurant)
+router.patch("/update/:id",auth,restaurantsCtrl.updateRestaurant)
 
 module.exports = router
